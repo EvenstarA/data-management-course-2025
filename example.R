9ff46763a013dcd4e27781028c37cf9ba9d3663b
EvenstarA <kmmarw001@myuct.ac.za>
2025-02-19 08:34:15 +0200

    created R script, quarto document and uploaded data

---
+++ example.R
@@ -0,0 +1,3 @@
+# Testing GitHub Tools with R Studio and an example data set.
+
+# The data set is sourced from a thrid year independent project at the University of Cape Town. (stored in exampled data_IP_...etc.). It contains species counts, species names, and quadrat data of ground cover and leaf litter features. 
